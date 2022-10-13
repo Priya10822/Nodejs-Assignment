43ed8e9f24c35137d229ff73e33a20a75e13edf1
Priya10822 <115445172+Priya10822@users.noreply.github.com>
2022-10-13 22:51:11 +0530

    Add files via upload

---
+++ fsmodule.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+//Reading file using readFile method
+fs.readFile("file.txt", "utf8", (err, data) => {
+  console.log(err, data);
+});
+
+console.log("Fininshed reading file");
+
+//blocking here
+//Reading file using readFileSync method
+const a = fs.readFileSync("file.txt");
+
+console.log(a.toString());
+
+console.log("Fininshed reading file");
+
+//Writing file using writeFile method
+fs.writeFile("file2.txt", "This is a data", () => {
+  console.log("Written to the file");
+});
+
+console.log("Fininshed writing file");
+
+//Writing file using writeFileSync method
+//blocking here
+const b = fs.writeFileSync("file2.txt", "This is a data2");
+
+console.log(b);
+
+console.log("Fininshed writing file");
